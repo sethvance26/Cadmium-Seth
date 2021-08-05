53d8e929ff04d036738d97ad4559c3710f79a6de
Seth <sethvance26@gmail.com>
2021-08-05 15:03:58 -0400

    deleting build folder again lol

--- server/server.js
+++ server/server.js
@@ -27,14 +27,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/index.html"));
-});
 
 db.once('open', () => {
   app.listen(PORT, () => {
