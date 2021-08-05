97ff0d2a8ef86cfa1dc8a8243e69b6ae0682eb5f
Seth <sethvance26@gmail.com>
2021-08-04 23:16:34 -0400

    deploying v3

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(favicon(__dirname + "/build/favicon.ico"));
+
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
@@ -26,12 +28,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 db.once('open', () => {
