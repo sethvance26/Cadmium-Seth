7c4c428abb76a42ca5739b48024d9e664b4acb89
Seth <sethvance26@gmail.com>
2021-08-23 15:21:47 -0400

    hardcoding lat and longs on map for some events

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -25,12 +25,36 @@ function Map() {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
-      <Marker latitude={37.54} longitude={-77.433} offsetLeft={-20} offsetTop={-10}>
+      <Marker latitude={37.60195} longitude={-77.56721} offsetLeft={-20} offsetTop={-10}>
         <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>Paint N' Sip (8/26) </p>
       </Marker>
-      <Marker latitude={37.52} longitude={-77.423} offsetLeft={-20} offsetTop={-10}>
+      <Marker latitude={37.75857} longitude={-77.48118} offsetLeft={-20} offsetTop={-10}>
         <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>Fourth Fridays (8/27) </p>
       </Marker>
+      <Marker latitude={37.52539} longitude={-77.43718} offsetLeft={-20} offsetTop={-10}>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>Dog Days of Summer (8/27) </p>
+      </Marker>
+      <Marker latitude={37.55262} longitude={-77.47776} offsetLeft={-20} offsetTop={-10}>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>Sirena's Gallery (8/27) </p>
+      </Marker>
+      <Marker latitude={37.53827} longitude={-77.44100}>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>Art N' Flow (9/04) </p>
+      </Marker>
+      <Marker latitude={37.50926} longitude={-77.46229}>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>RVA Exotic Paintings (9/12) </p> 
+      </Marker>
+      <Marker latitude={37.58512} longitude={-77.49300}>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <p>All Media Show (9/17) </p> 
+       
+      </Marker>
+      
 
     </ReactMapGL>
     </div>
